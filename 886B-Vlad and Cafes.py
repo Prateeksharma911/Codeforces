dffc034177ad660339c1934584177f35a6e5c204
Prateeksharma911 <prateek1999sharma@gmail.com>
2021-05-13 16:20:04 +0530

    886Bchange

--- 886B-Vlad and Cafes.py
+++ 886B-Vlad and Cafes.py
@@ -1,9 +1,8 @@
 n=int(input())
 x=list(map(int,input().split()))
-distfind = [n for i in range(n)]
-
-for i in range(n):
-    distfind[x[i]-1]=i
-
+distfind = [n for i in range(20000000)]
+minimumnum=n
+for i in range(len(x)):
+    distfind[x[i]]=i
 distfind.sort()
 print(x[distfind[0]])
