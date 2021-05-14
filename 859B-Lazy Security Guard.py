5d7f46a1d3176e222141bbcc9eb7bcf7c53d8e9c
Prateeksharma911 <prateek1999sharma@gmail.com>
2021-05-14 22:50:22 +0530

    859B-Lazy Security Guard

---
+++ 859B-Lazy Security Guard.py
@@ -0,0 +1,21 @@
+import math
+n=int(input())
+total=0
+if n==1:
+    print(4)
+elif n==2:
+    print(6)
+else:
+    s=math.sqrt(n)
+    snumber=math.floor(s)
+    block=snumber*4
+    noofblock=snumber**2
+    noofblock=n-noofblock
+    if noofblock>1:
+
+        x=noofblock//snumber
+        f=math.floor(noofblock/(snumber+1))+1
+        block+=f*2
+    elif noofblock==1:
+        block+=2
+    print(block)
